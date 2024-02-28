5662c3f9b74077a06d86376264a4dbd6989f8a62
leonardoparra <leonardocabrices1@gmail.com>
2024-02-28 17:08:16 -0400

    creacion modelo review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_28_193832) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_28_210342) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,12 +28,21 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_28_193832) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.integer "rating"
     t.float "price"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,4 +62,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_28_193832) do
   add_foreign_key "checkouts", "products"
   add_foreign_key "checkouts", "users"
   add_foreign_key "products", "users"
+  add_foreign_key "reviews", "products"
+  add_foreign_key "reviews", "users"
 end
