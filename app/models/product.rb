f0eb7228a9ada843b179cebe2ed944939008d0d9
andres torres <dragotorres16@gmail.com>
2024-02-28 16:41:46 -0400

    revision de controlador product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :checkout
+  validates :name, presence: :true
+  validates :price, presence: :true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :products, dependent: :destroy
+  has_many :checkout
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
