9e03cbe200f47f0b74688703ee2e9935b71652c1
LeonardoParra <leonardocabrices1@gmail.com>
2024-02-29 11:22:45 -0400

    cambios en el navbar, migraciones y configuracion cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :products, dependent: :destroy
   has_many :checkout
+  has_one_attached :photo
   validates :name, presence: :true
   validates :last_name, presence: :true
   # Include default devise modules. Others available are:
