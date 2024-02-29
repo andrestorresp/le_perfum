b6fcef3e32b58e6258f72fd51a1d8283c9245eb1
LeonardoParra <leonardocabrices1@gmail.com>
2024-02-29 10:22:09 -0400

    cambios en el modelo user phone_address

---
+++ db/migrate/20240229140738_add_column_phone_and_address_to_user.rb
@@ -0,0 +1,6 @@
+class AddColumnPhoneAndAddressToUser < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :phone_number, :string
+    add_column :users, :address, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_28_210342) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_29_140738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_28_210342) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
